6a82666e5e04cc30d20b2e29c86b957febe37ac6
jordan <jpburkhardt@ucsd.edu>
2022-05-26 13:04:05 -0700

    Adding download.sh file to download necessary datasets

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E9.5.rds
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E10.5.rds
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E11.5.rds
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E12.5.rds
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E13.5.rds
+wget https://shendure-web.gs.washington.edu/content/members/cxqiu/public/nobackup/tome_summary_data/mm/seurat_object_E8.5b.rds
